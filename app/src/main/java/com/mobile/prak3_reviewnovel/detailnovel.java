9192c6e888ef67431bbd02c51e4963c01d976c93
Amelya25 <guardian.gaia3@gmail.com>
2022-10-23 21:41:58 +0700

    View and ViewGroup

---
+++ app/src/main/java/com/mobile/prak3_reviewnovel/detailnovel.java
@@ -0,0 +1,15 @@
+package com.mobile.prak3_reviewnovel;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.view.View;
+import android.os.Bundle;
+
+public class detailnovel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailnovel);
+    }
+}
